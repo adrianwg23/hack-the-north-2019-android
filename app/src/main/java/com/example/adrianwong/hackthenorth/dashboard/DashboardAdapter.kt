acf81e463a8eb75ba16b2ae3c1655fc8eb57aa0e
TanjotP <tanjotspanesar@gmail.com>
2019-09-15 07:33:24 -0400

    help pls

--- app/src/main/java/com/example/adrianwong/hackthenorth/dashboard/DashboardAdapter.kt
+++ app/src/main/java/com/example/adrianwong/hackthenorth/dashboard/DashboardAdapter.kt
@@ -10,16 +10,17 @@ import com.example.adrianwong.hackthenorth.R
 import com.example.adrianwong.hackthenorth.datamodels.CurrentPool
 import kotlinx.android.synthetic.main.dashboard_row.view.*
 import android.widget.TextView
-import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.dashboard_all_pool.view.*
+
+import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.dashboard_row.view.live_count
 
 
-class ChapterAdapter(
+class DashboardAdapter(
     private val chaptersList: ArrayList<CurrentPool>,
     private val context: Context?
 ) :
-    RecyclerView.Adapter<ChapterAdapter.ViewHolder>() {
+    RecyclerView.Adapter<DashboardAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
             LayoutInflater.from(context).inflate(
@@ -31,8 +32,7 @@ class ChapterAdapter(
     }
 
     override fun getItemCount(): Int {
-        //return chaptersList.size
-        return 17
+        return chaptersList.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -42,34 +42,8 @@ class ChapterAdapter(
     }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val chapterName = view.live_count
-        val chapterDate = view.date
-    }
-}
-
-class DashboardAdapter(val items: ArrayList<CurrentPool>, val context: Context?) :
-    RecyclerView.Adapter<DashboardAdapter.ViewHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(
-            LayoutInflater.from(context).inflate(
-                R.layout.dashboard_row,
-                parent,
-                false
-            )
-        )
+        val chapterName = itemView.live_count
+        val chapterDate = itemView.date
     }
 
-    override fun getItemCount(): Int = items.size
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        items?.let { v ->
-            holder.title.text = v[position].totalAmount.toString()
-        }
-    }
-
-
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val title = itemView.live_count
-    }
 }
--- app/src/main/java/com/example/adrianwong/hackthenorth/datamodels/AllPoolWrapper.kt
+++ app/src/main/java/com/example/adrianwong/hackthenorth/datamodels/AllPoolWrapper.kt
@@ -3,7 +3,7 @@ package com.example.adrianwong.hackthenorth.datamodels
 import com.google.gson.annotations.SerializedName
 
 data class AllPoolWrapper(
-    var arrayList: ArrayList<CurrentPool>
+    var allPoolArrayList: ArrayList<CurrentPool>
 )
 
 data class ResultWrapper(
