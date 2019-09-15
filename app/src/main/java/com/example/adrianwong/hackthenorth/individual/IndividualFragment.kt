d9b188a502509f7eb3c347ea405bb4cc10cf519b
TanjotP <tanjotspanesar@gmail.com>
2019-09-14 20:40:40 -0400

    some shit idk

--- app/src/main/java/com/example/adrianwong/hackthenorth/individual/IndividualFragment.kt
+++ app/src/main/java/com/example/adrianwong/hackthenorth/individual/IndividualFragment.kt
@@ -1,11 +1,16 @@
 package com.example.adrianwong.hackthenorth.individual
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.adrianwong.hackthenorth.R
+import kotlinx.android.synthetic.main.fragment_individual.*
+import android.app.Activity
+import android.R.attr.data
+import com.example.adrianwong.hackthenorth.BarCodeScannerActivity
 
 
 class IndividualFragment : Fragment() {
@@ -16,6 +21,14 @@ class IndividualFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_individual, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        openScanner.setOnClickListener {
+            val intent = Intent(activity, BarCodeScannerActivity::class.java)
+            activity?.startActivity(intent)
+        }
+        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/example/adrianwong/hackthenorth/repository/BarcodeScanningModel.kt
@@ -0,0 +1,3 @@
+package com.example.adrianwong.hackthenorth.repository
+
+data class BarcodeScanningModel(val id: Int, val text: String?)
